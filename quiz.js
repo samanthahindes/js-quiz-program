691e8f9c842db12cbf173f19ae546dd18e2012c6
samanthahindes <samantha.hindes@wustl.edu>
2016-04-13 18:06:48 -0500

    quiz

--- quiz.js
+++ quiz.js
@@ -2,13 +2,39 @@
 var correct = 0;
 
 // prompt
-var answer1 = prompt("ask a question...");
+var answer1 = prompt("What is the town that the hit series Gilmore Girls takes place?");
 console.log("the answer is:" + answer1);
 
+var answer2= prompt("What is the name of the high school Rory attends?");
+
+var answer3= prompt("Who owns the diner that Lorelai and Rory frequently eat at?");
+
+var answer4= prompt("What is the name of the inn that Lorelai owns?");
+
+var answer5= prompt("What is the name of the Rory's first boyfriend?");
+
 // if statement
-if ( answer1.toUpperCase() === 'MAGIC' ) {
+if ( answer1.toUpperCase() === 'STARS HOLLOW' ) {
  correct += 1;
 }
 
+if ( answer2.toUpperCase() === 'CHILTON' ) {
+ correct += 1;
+}
+
+if ( answer3.toUpperCase() === 'LUKE' ) {
+ correct += 1;
+}
+
+if ( answer4.toUpperCase() === 'THE DRAGONFLY INN' ) {
+ correct += 1;
+}
+
+if ( answer5.toUpperCase() === 'DEAN' ) {
+ correct += 1;
+}
+
+console.log("correct:" + correct);
+
 // output results
-document.write("<p>write something to page...</p>");
+document.write("<p>You got"+ correct + "correct</p>");
